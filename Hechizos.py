d7f0ccbb4e8e8d82b2757a3e8936fc9c6cb17ee5
CharlyGoffres <carlesruiz1999@gmail.com>
2023-01-05 12:45:52 +0100

    2023-01-05 Començar a fer la funcio hechizos.json

--- Hechizos.py
+++ Hechizos.py
@@ -1,60 +1,46 @@
-spells = {
-  "Fireball": Spell("Fireball", "Fire", 5, 50),
-  "Lightning Bolt": Spell("Lightning Bolt", "Thunder", 4, 40),
-  "Frostbite": Spell("Frostbite", "Ice", 3, 30)
-}
-class Spell:
-  def __init__(self, name, magic_type, level, damage):
-    self.name = name
-    self.magic_type = magic_type
-    self.level = level
-    self.damage = damage
 
-class MagicSystem:
-  def __init__(self, max_uses_per_level):
-    self.max_uses_per_level = max_uses_per_level
-    self.uses_left = max_uses_per_level.copy()
+import json
 
-  def use(self, level):
-    if self.uses_left[level] > 0:
-      self.uses_left[level] -= 1
-      return True
+# Load the JSON file
+with open("character.json", "r") as f:
+    data = json.load(f)
+
+# Extract the hit, saving_throw, and damage_dice fields from the first spell in the list
+
+hit = data["hechizos"][0]["damage"]["hit"]
+saving_throw = data["hechizos"][0]["damage"]["saving_throw"]
+damage_dice = data["hechizos"][0]["damage"]["damage_dice"]
+
+# Print the extracted fields
+print("Hit:", hit)
+print("Saving throw:", saving_throw)
+print("Damage dice:", damage_dice)
+
+
+
+
+#per fer que es modifiqui del archiu json
+import json
+
+# Load the JSON file
+with open("character.json", "r") as f:
+    data = json.load(f)
+
+# Get the maximum number of level 1 spells and the remaining number of uses
+max_level_1_spells = data["spell_slots"]["spell_level_1_max"]
+remaining_level_1_uses = data["spell_slots"]["spell_level_1"]
+
+# Function to use a level 1 spell
+def use_level_1_spell():
+    global remaining_level_1_uses
+    if remaining_level_1_uses > 0:
+        # Use a level 1 spell
+        remaining_level_1_uses -= 1
+        print("Used a level 1 spell. Remaining level 1 spells:", remaining_level_1_uses)
+        # Update the "spell_level_1" field in the JSON file
+        data["spell_slots"]["spell_level_1"] = remaining_level_1_uses
+        with open("character.json", "w") as f:
+            json.dump(data, f)
     else:
-      return False
-
-  def set_uses(self, level, uses):
-    self.uses_left[level] = uses
-
-magic_system = MagicSystem({1: 2, 2: 3, 3: 4, 4: 5, 5: 6})
-
-
-#Canviar el nombre de usos dels hechizos per level
-magic_system.set_uses(fireball.level, 1)
-
-if magic_system.use(fireball.level):
-  # Lanzar hechizo fireball
-  print(f"El hechizo {fireball.name} ha sido lanzado con éxito!")
-if magic_system.use(fireball.level):
-  print("No tienes más usos de magia de este nivel!")
-
-
-
-print(spells["Fireball"].damage)
-print(spells["Lightning Bolt"].damage)
-print(spells["Frostbite"].damage)
-
-
-def __init__(self, nombre, tipo, nivel):
-        # Verificar que el nivel del hechizo esté dentro de los límites permitidos
-        if nivel not in self.LIMITE_HECHIZOS:
-            raise ValueError(f"Nivel de hechizo inválido: {nivel}")
-        
-        self.nombre = nombre
-        self.tipo = tipo
-        self.nivel = nivel
-    
-    def definir_dano(self):
-        # Calcular el daño en función del nivel del hechizo
-        self.dano = 10 * self.nivel
-        
-        print(f"Hechizo usado: {self.nombre} (nivel {self.nivel}, daño {self.dano})")
+        # Cannot use a level 1 spell
+        print("No remaining level 1 spells")
